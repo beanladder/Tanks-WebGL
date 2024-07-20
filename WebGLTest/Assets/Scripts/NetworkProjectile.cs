e76c65fd50873065daed5b97aeaf63ba29fa84fd
Angshu Chatterjee <angshu169@gmail.com>
2024-07-20 19:30:35 +0530

    back to aukat

--- WebGLTest/Assets/Scripts/NetworkProjectile.cs
+++ WebGLTest/Assets/Scripts/NetworkProjectile.cs
@@ -85,86 +85,29 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class NetworkProjectile : MonoBehaviourPunCallbacks
+public class NetworkProjectile : MonoBehaviour
 {
     public AudioSource Boom;
     public AudioSource[] Ricochet;
     public GameObject TankHitAudio;
-    public GameObject boomPrefab;
-    public GameObject hitwallPrefab;
+    public GameObject boomPrefab; // Prefab to instantiate when hitting a tank
+    public GameObject hitwallPrefab; // Prefab to instantiate when hitting a wall/prop
     private int damageAmt;
-    public float maxLifetime = 5f;
-    private float lifetimeTimer;
-    private bool hasHitFinalTarget = false;
-    private int shooterViewID;
-    
-
-    public void InitializeProjectile(int viewID)
-    {
-        shooterViewID = viewID;
-        lifetimeTimer = 0f;
-        hasHitFinalTarget = false;
-        EnableRenderers(true);
-        Debug.Log($"Projectile initialized: {gameObject.name} at {transform.position}");
-    }
-
-    private void Update()
-    {
-        if (!hasHitFinalTarget)
-        {
-            lifetimeTimer += Time.deltaTime;
-            if (lifetimeTimer >= maxLifetime)
-            {
-                DisableProjectile();
-            }
-            Debug.DrawRay(transform.position, GetComponent<Rigidbody>().velocity.normalized * 5f, Color.red);
-        }
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"Projectile collided: {gameObject.name} with {collision.gameObject.name} at {collision.contacts[0].point}");
         HandleProjectileCollision(collision);
     }
 
     private void HandleProjectileCollision(Collision collision)
     {
-        if (hasHitFinalTarget) return;
-
         if (collision.gameObject.CompareTag("Cylinder"))
         {
-            HandleCylinderCollision(collision);
+            gameObject.GetComponent<Renderer>().enabled = true;
+            StartCoroutine(PlayAudio("Ricochet"));
         }
-        else if (collision.gameObject.CompareTag("Tank") ||
-                 collision.gameObject.CompareTag("Wall") ||
-                 collision.gameObject.CompareTag("Ground"))
+        else if (collision.gameObject.CompareTag("Tank"))
         {
-
-            hasHitFinalTarget = true;
-            DisableProjectile(); // Immediately disable the projectile
-            HandleFinalCollision(collision);
-        }
-    }
-
-    private void HandleCylinderCollision(Collision collision)
-    {
-        EnableRenderers(true);
-        StartCoroutine(PlayAudio("Ricochet"));
-
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            Vector3 reflectedVelocity = Vector3.Reflect(rb.velocity, collision.contacts[0].normal);
-            rb.velocity = reflectedVelocity * 0.8f;
-        }
-    }
-
-    private void HandleFinalCollision(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Tank"))
-        {
-            HandleTankCollision(collision);
-
             gameObject.GetComponentInChildren<TrailRenderer>().enabled = false;
             gameObject.GetComponent<Renderer>().enabled = false;
             GameObject explosion = Instantiate(boomPrefab, collision.contacts[0].point, Quaternion.identity);
@@ -186,100 +129,19 @@ public class NetworkProjectile : MonoBehaviourPunCallbacks
             }
             //collision.gameObject.GetComponent<TankInfo>().TakeDamage(DamageAmt);
             Destroy(gameObject);
-
         }
         else if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Ground"))
         {
-            HandleWallOrGroundCollision(collision);
-        }
-
-        StartCoroutine(DelayedCleanup());
-    }
-
-    private void HandleTankCollision(Collision collision)
-    {
-        GameObject explosion = Instantiate(boomPrefab, collision.contacts[0].point, Quaternion.identity);
-        Destroy(explosion, 2f);
-        PlayAudioAtPoint(TankHitAudio, collision.contacts[0].point);
-        damageAmt = Random.Range(4, 9);
-
-        PhotonView targetView = collision.gameObject.GetComponent<PhotonView>();
-        if (targetView != null)
-        {
-            targetView.RPC("TakeDamage", RpcTarget.All, damageAmt, shooterViewID);
-        }
-    }
-
-    private void HandleWallOrGroundCollision(Collision collision)
-    {
-        GameObject explosion = Instantiate(hitwallPrefab, collision.contacts[0].point, Quaternion.identity);
-        Destroy(explosion, 2f);
-        PlayAudioAtPoint(TankHitAudio, collision.contacts[0].point);
-    }
-
-    private void PlayAudioAtPoint(GameObject audioPrefab, Vector3 position)
-    {
-        GameObject audioCont = Instantiate(audioPrefab, position, Quaternion.identity);
-        AudioSource audioSrc = audioCont.GetComponent<AudioSource>();
-        audioSrc.Play();
-        Destroy(audioCont, 2f);
-    }
-
-    private IEnumerator DelayedCleanup()
-    {
-        yield return new WaitForSeconds(2f);
-        ReturnToPool();
-    }
-
-    private void DisableProjectile()
-    {
-        
-        EnableRenderers(false);
-        GetComponent<Collider>().enabled = false;
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.velocity = Vector3.zero;
-            rb.isKinematic = true;
-        }
-    }
-
-    private void EnableRenderers(bool enable)
-    {
-        MeshRenderer renderer = GetComponent<MeshRenderer>();
-        if (renderer != null) renderer.enabled = enable;
-
-        TrailRenderer trail = GetComponentInChildren<TrailRenderer>();
-        if (trail != null) trail.enabled = enable;
-
-        ParticleSystem particle = GetComponentInChildren<ParticleSystem>();
-        //if(particle != null) 
-    }
-
-    private void ReturnToPool()
-    {
-        Debug.Log($"Projectile returned to pool: {gameObject.name}");
-        if (ProjectilePool.Instance != null)
-        {
-            ProjectilePool.Instance.ReturnToPool(gameObject);
-        }
-        else
-        {
+            gameObject.GetComponentInChildren<TrailRenderer>().enabled = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
+            GameObject explosion = Instantiate(hitwallPrefab, collision.contacts[0].point, Quaternion.identity);
+            Destroy(explosion, 2f);
+            GameObject audioCont = Instantiate(TankHitAudio, collision.contacts[0].point, Quaternion.identity);
+            AudioSource audioSrc = audioCont.GetComponent<AudioSource>();
+            audioSrc.Play();
+            Destroy(audioCont, 2f);
             Destroy(gameObject);
         }
-    }
-
-    public void ResetProjectile()
-    {
-        hasHitFinalTarget = false;
-        EnableRenderers(true);
-        GetComponent<Collider>().enabled = true;
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.isKinematic = false;
-        }
-        lifetimeTimer = 0f;
     }
 
     public IEnumerator PlayAudio(string ID)
@@ -288,6 +150,7 @@ public class NetworkProjectile : MonoBehaviourPunCallbacks
         {
             Boom.Play();
             yield return new WaitForSeconds(0.5f);
+            Destroy(gameObject);
         }
         else if (ID == "Ricochet")
         {
--- WebGLTest/Assets/Scripts/NetworkProjectileLauncher.cs
+++ WebGLTest/Assets/Scripts/NetworkProjectileLauncher.cs
@@ -4,154 +4,112 @@ using Photon.Pun;
 using Unity.VisualScripting;
 using UnityEngine;
 
-
 public class NetworkProjectileLauncher : MonoBehaviourPunCallbacks
 {
     public GameObject projectilePrefab;
-    public GameObject barrelSmokePrefab;
-    public GameObject barrelFlashPrefab;
-    public GameObject smokeGrenadePrefab;
+    public GameObject BarrelSmokePrefab;
+    public GameObject BarrelFlashPrefab;
+    public GameObject SmokeGrenadePrefab;
+    public GameObject trailPrefab;
     public Transform firePoint;
     public GameObject turret;
     public GameObject target;
+    public float force = 10f;
     public float speed = 5f;
     public float recoilDistance = 0.1f;
     public float recoilDuration = 0.1f;
+    public float trailDuration = 2f;
     public float cooldownTime = 1f;
 
-    public AudioClip fireSound;
-    public AudioClip reloadSound;
+    public GameObject reloadSoundObject; // Reference to the empty GameObject with the reload sound AudioSource
 
+    private AudioSource audioSource;
     private Vector3 originalTurretPosition;
     private bool canFire = true;
-    private PhotonView view;
-    private AudioSource audioSource;
+    private Coroutine cooldownCoroutine;
+    PhotonView view;
+    public static NetworkProjectileLauncher instance;
 
-    private ObjectPool projectilePool;
-    private ObjectPool barrelSmokePool;
-    private ObjectPool barrelFlashPool;
-    private ObjectPool smokeGrenadePool;
-
-    void Awake()
+    private void Awake()
     {
+        instance = this;
+    }
+
+    void Start()
+    {
+        originalTurretPosition = turret.transform.localPosition;
         view = GetComponent<PhotonView>();
         audioSource = GetComponent<AudioSource>();
-        originalTurretPosition = turret.transform.localPosition;
-
-        // Initialize object pools
-        projectilePool = new ObjectPool(projectilePrefab, 10);
-        barrelSmokePool = new ObjectPool(barrelSmokePrefab, 5);
-        barrelFlashPool = new ObjectPool(barrelFlashPrefab, 5);
-        smokeGrenadePool = new ObjectPool(smokeGrenadePrefab, 5);
     }
 
     void Update()
     {
-        if (!view.IsMine) return;
-
-        if (Input.GetMouseButtonDown(0) && canFire)
+        if (view.IsMine)
         {
-            photonView.RPC("FireProjectile", RpcTarget.All);
-        }
-        else if (Input.GetMouseButtonDown(1) && canFire)
-        {
-            FireSmokeGrenade();
-        }
-    }
-
-    [PunRPC]
-    void FireProjectile()
-    {
-        if (!canFire) return;
-
-        GameObject projectile = projectilePool.GetObject();
-        if (projectile != null)
-        {
-            projectile.SetActive(true);
-            projectile.transform.position = firePoint.position;
-            projectile.transform.rotation = firePoint.rotation;
-
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            if (rb == null) rb = projectile.AddComponent<Rigidbody>();
-
-            rb.isKinematic = false;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-
-            Vector3 direction = (target.transform.position - firePoint.position).normalized;
-            rb.AddForce(direction * speed, ForceMode.VelocityChange);
-
-            NetworkProjectile networkProjectile = projectile.GetComponent<NetworkProjectile>();
-            if (networkProjectile != null)
+            if (Input.GetMouseButtonDown(0) && canFire)
             {
-                networkProjectile.InitializeProjectile(view.ViewID);
+                view.RPC("FireProjectile", RpcTarget.All);
+                view.RPC("RecoilAnimation", RpcTarget.All);
+                if (audioSource != null)
+                {
+                    audioSource.Play();
+                }
+                cooldownCoroutine = StartCoroutine(Cooldown());
             }
-
-            if (PhotonNetwork.IsMessageQueueRunning)
+            else if (Input.GetMouseButtonDown(1) && canFire)
             {
-                photonView.RPC("SyncProjectile", RpcTarget.Others, projectile.GetComponent<PhotonView>().ViewID, firePoint.position, direction);
-            }
-        }
-
-        SpawnEffect(barrelSmokePool);
-        SpawnEffect(barrelFlashPool);
-        PlaySound(fireSound);
-        RecoilAnimation();
-        StartCoroutine(Cooldown());
-    }
-
-    [PunRPC]
-    void SyncProjectile(int viewID, Vector3 position, Vector3 direction)
-    {
-        PhotonView projectileView = PhotonView.Find(viewID);
-        if (projectileView != null)
-        {
-            GameObject projectile = projectileView.gameObject;
-            projectile.transform.position = position;
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.velocity = direction * speed;
+                view.RPC("FireSmokeGrenade", RpcTarget.All);
+                view.RPC("RecoilAnimation", RpcTarget.All);
+                if (audioSource != null)
+                {
+                    audioSource.Play();
+                }
+                cooldownCoroutine = StartCoroutine(Cooldown());
             }
         }
     }
 
-    [PunRPC]
-    void FireSmokeGrenade()
+    IEnumerator Cooldown()
     {
-        if (!canFire) return;
+        if (reloadSoundObject != null)
+        {
+            AudioSource reloadAudioSource = reloadSoundObject.GetComponent<AudioSource>();
+            if (reloadAudioSource != null)
+            {
+                reloadAudioSource.Play();
+            }
+        }
+        canFire = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canFire = true;
+    }
 
-        GameObject smokeGrenade = smokeGrenadePool.GetObject();
-        smokeGrenade.transform.position = firePoint.position;
-        smokeGrenade.transform.rotation = firePoint.rotation;
+    [PunRPC]
+    public void FireProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        GameObject trailEffect = Instantiate(trailPrefab, firePoint.position, firePoint.rotation);
+        trailEffect.transform.parent = projectile.transform;
 
         Vector3 direction = (target.transform.position - firePoint.position).normalized;
-        Rigidbody grenadeRb = smokeGrenade.GetComponent<Rigidbody>();
-        if (grenadeRb != null)
+
+        Rigidbody projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        if (projectileRigidbody != null)
         {
-            grenadeRb.velocity = direction * speed;
+            projectileRigidbody.velocity = direction * speed;
+            projectileRigidbody.angularDrag = 1f;
+            projectileRigidbody.drag = projectileRigidbody.angularDrag - 0.5f;
         }
 
-        PlaySound(fireSound);
-        RecoilAnimation();
-        StartCoroutine(Cooldown());
+        Destroy(trailEffect, trailDuration);
+        GameObject smoke = Instantiate(BarrelSmokePrefab, firePoint.position, firePoint.rotation);
+        GameObject flash = Instantiate(BarrelFlashPrefab, firePoint.position, firePoint.rotation);
+        Destroy(flash, 2f);
+        Destroy(smoke, 2f);
     }
 
-    void SpawnEffect(ObjectPool pool)
-    {
-        GameObject effect = pool.GetObject();
-        effect.transform.position = firePoint.position;
-        effect.transform.rotation = firePoint.rotation;
-        StartCoroutine(ReturnToPool(effect, pool, 2f));
-    }
-
-    IEnumerator ReturnToPool(GameObject obj, ObjectPool pool, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        pool.ReturnObject(obj);
-    }
-
-    void RecoilAnimation()
+    [PunRPC]
+    public void RecoilAnimation()
     {
         LeanTween.moveLocalZ(turret, originalTurretPosition.z - recoilDistance, recoilDuration)
             .setEase(LeanTweenType.easeOutQuad)
@@ -162,58 +120,18 @@ public class NetworkProjectileLauncher : MonoBehaviourPunCallbacks
             });
     }
 
-    IEnumerator Cooldown()
+    [PunRPC]
+    public void FireSmokeGrenade()
     {
-        canFire = false;
-        PlaySound(reloadSound);
-        yield return new WaitForSeconds(cooldownTime);
-        canFire = true;
-    }
+        GameObject smokeGrenade = Instantiate(SmokeGrenadePrefab, firePoint.position, firePoint.rotation);
+        Vector3 direction = (target.transform.position - firePoint.position).normalized;
 
-    void PlaySound(AudioClip clip)
-    {
-        if (audioSource != null && clip != null)
+        Rigidbody grenadeRigidbody = smokeGrenade.GetComponent<Rigidbody>();
+        if (grenadeRigidbody != null)
         {
-            audioSource.PlayOneShot(clip);
+            grenadeRigidbody.velocity = direction * speed;
+            grenadeRigidbody.angularDrag = 1f;
+            grenadeRigidbody.drag = grenadeRigidbody.angularDrag - 0.5f;
         }
-    }
-}
-
-// Helper class for object pooling
-public class ObjectPool
-{
-    private GameObject prefab;
-    private Queue<GameObject> pool;
-
-    public ObjectPool(GameObject prefab, int initialSize)
-    {
-        this.prefab = prefab;
-        pool = new Queue<GameObject>();
-
-        for (int i = 0; i < initialSize; i++)
-        {
-            GameObject obj = Object.Instantiate(prefab);
-            obj.SetActive(false);
-            pool.Enqueue(obj);
-        }
-    }
-
-    public GameObject GetObject()
-    {
-        if (pool.Count == 0)
-        {
-            GameObject obj = Object.Instantiate(prefab);
-            return obj;
-        }
-
-        GameObject pooledObject = pool.Dequeue();
-        pooledObject.SetActive(true);
-        return pooledObject;
-    }
-
-    public void ReturnObject(GameObject obj)
-    {
-        obj.SetActive(false);
-        pool.Enqueue(obj);
     }
 }
