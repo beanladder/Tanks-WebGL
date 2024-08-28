5557ca4b42cdd9a0f6030e581f7b353c0aa1df37
Angshu Chatterjee <angshu169@gmail.com>
2024-08-28 14:36:06 +0530

    added files

---
+++ WebGLTest/SquareTanksOffline/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Doorlight Studios-Square Tanks-0.1b";
+const contentToCache = [
+    "Build/SquareTanksOffline.loader.js",
+    "Build/SquareTanksOffline.framework.js.gz",
+    "Build/SquareTanksOffline.data.gz",
+    "Build/SquareTanksOffline.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
